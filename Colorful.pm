387c5bf23e4c6d8dc31b0e4c70eb74da16482e22
Joshua Eric Turcotte <joshua.eric.turcotte@gmail.com>
2015-11-16 19:16:39 -0500

    The previous layout refused to work...

--- Colorful.pm
+++ Colorful.pm
@@ -1,25 +1,24 @@
-package Colorful {
+package Colorful;
 
-	use feature 'say';
-	use Exporter;
-	use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-	
-	$VERSION = 0.01;
-	@ISA = qw(Exporter);
-	@EXPORT = qw(panic grin whine);
+use strict;
+use feature 'say';
+use Exporter;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-	sub panic {
-		die "\x1b[1m\x1b[31m".(shift || 'Fatal error')."\x1b[0m\n";
-	}	
+    $VERSION = 0.1;
+    @ISA = qw(Exporter);
+    @EXPORT = qw(panic grin whine);
 
-	sub grin {
-		say "\x1b[32m".shift."\x1b[0m";
-	}
+    sub panic {
+        die "\x1b[1m\x1b[31m".(shift || 'Fatal error')."\x1b[0m\n";
+    }
 
-	sub whine {
-		say "\x1b[33m".(shift || 'Unknown error')."\x1b[0m";
-	}
+    sub grin {
+        say "\x1b[32m".shift."\x1b[0m";
+    }
 
-}
+    sub whine {
+        say "\x1b[33m".(shift || 'Unknown error')."\x1b[0m";
+    }
 
 1;
